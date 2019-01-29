24c01f824334ae9e7c1014a0ed799628a2ad69c3
Leonov Artem <leonovru@yandex.ru>
2019-01-29 16:24:38 +0300

    Add method 'Swap'

--- Heap Sort/Heap.cs
+++ Heap Sort/Heap.cs
@@ -12,6 +12,11 @@ namespace Heap_Sort
 
         public Heap(T[] array) => this.array = array;
 
-
+        private void Swap(int first_index, int second_index)
+        {
+            T temp = array[first_index];
+            array[first_index] = array[second_index];
+            array[second_index] = temp;
+        }
     }
 }
