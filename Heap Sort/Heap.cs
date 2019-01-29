a3fe51414b41a224ee039c19d6a3897221ef9963
Leonov Artem <leonovru@yandex.ru>
2019-01-29 16:37:53 +0300

    Add method 'BuidHeap'

--- Heap Sort/Heap.cs
+++ Heap Sort/Heap.cs
@@ -12,6 +12,22 @@ namespace Heap_Sort
 
         public Heap(T[] array) => this.array = array;
 
+        private void BuidHeap(int size)
+        {
+            for (int i = size - 1; i >= 0; i--)
+            {
+                int child = i;
+
+                while (child != 0)
+                {
+                    int parent = (child - 1) / 2;                           // индекс родительской записи
+                    if (array[parent].CompareTo(array[child]) >= 0) break;  // если свойство выполняется, то выходим из цикла
+                    Swap(parent, child);                                    // иначе меняем местами родительскую и дочернюю записи
+
+                    child = parent;                                         // переходим к родительской записи
+                }
+            }
+        }
         private void Swap(int first_index, int second_index)
         {
             T temp = array[first_index];
